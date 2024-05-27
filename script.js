98c73a8f36725b3bf8d285463a0374409c2a85b4
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-05-28 00:53:54 +0200

    rassemblement

--- script.js
+++ script.js
@@ -10,32 +10,32 @@ let correctAnswer = 0;
 
 // I use forEach to loop through the array and match all elements with the class true and sum 1 to correctAnswer everytime a correct answer is clicked
 answerCheck.forEach((checkbox) => {
-  checkbox.addEventListener("click", () => {
-    if (checkbox.classList.contains("true")) {
-      correctAnswer++;
-      counter.textContent = correctAnswer;
-      console.log("I am checked with a loop");
-      console.log(correctAnswer);
-    }
-  });
+    checkbox.addEventListener("click", () => {
+        if (checkbox.classList.contains("true")) {
+            correctAnswer++;
+            counter.textContent = correctAnswer;
+            console.log("I am checked with a loop");
+            console.log(correctAnswer);
+        }
+    });
 });
 
 function blockCheck(el) {
-  const checkbox = document.querySelectorAll(".question1");
-  checkbox.forEach((box) => (box.disabled = "disabled"));
+    const checkbox = document.querySelectorAll(".question1");
+    checkbox.forEach((box) => (box.disabled = "disabled"));
 }
 
 function blockCheck2(el) {
-  const checkbox = document.querySelectorAll(".question2");
-  checkbox.forEach((box) => (box.disabled = "disabled"));
+    const checkbox = document.querySelectorAll(".question2");
+    checkbox.forEach((box) => (box.disabled = "disabled"));
 }
 
 function blockCheck3(el) {
-  const checkbox = document.querySelectorAll(".question3");
-  checkbox.forEach((box) => (box.disabled = "disabled"));
+    const checkbox = document.querySelectorAll(".question3");
+    checkbox.forEach((box) => (box.disabled = "disabled"));
 }
 
 function blockCheck4(el) {
-  const checkbox = document.querySelectorAll(".question4");
-  checkbox.forEach((box) => (box.disabled = "disabled"));
+    const checkbox = document.querySelectorAll(".question4");
+    checkbox.forEach((box) => (box.disabled = "disabled"));
 }
