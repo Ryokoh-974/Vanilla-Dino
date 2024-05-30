a77c9beeebbcfc2d9101d1296e098d72cd744c3d
anthony gelormini <gelo2b@outlook.fr>
2024-05-30 11:59:04 +0200

    introduction du code js pour l emoticone like

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ function blockCheck4(el) {
     const checkbox = document.querySelectorAll(".question4");
     checkbox.forEach((box) => (box.disabled = "disabled"));
 }
+
+/*parti anthony like(coeur-anim)*/ 
+
+const like = document.querySelector('.like');
+
+let countLike = 0;
+like.addEventListener('click', () => {
+
+    if(countLike === 0) {
+        like.classList.toggle('anim-like');
+        countLike = 1;
+        like.style.backgroundPosition = 'right';
+    } else {
+        countLike = 0;
+        like.style.backgroundPosition = 'left';
+    }
+
+});
